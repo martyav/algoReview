749dc0c8dbc9d3adb65e4891f90324696919d559
Marty A/V <martyavedon@gmail.com>
2018-07-13 18:57:49 -0700

    Create booWho.js

---
+++ javascriptSolutions/booWho.js
@@ -0,0 +1,5 @@
+// Check if a value is classified as a boolean primitive. Return true or false. Truthy and falsey values should return false.
+
+function booWho(bool) {
+  return typeof bool === "boolean";
+}
