17f204f48500aab6f924345851566917c1ad93ca
Marty A/V <martyavedon@gmail.com>
2018-07-13 18:28:08 -0700

    Create repeatStringRecursively.js

---
+++ javascript/repeatStringRecursively.js
@@ -0,0 +1,13 @@
+// Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number.
+
+function repeatStringNumTimes(str, num) {
+  if (num <= 0) {
+    return "";
+  }
+
+  if (num === 1) {
+    return str;
+  }
+
+  return str + repeatStringNumTimes(str, num - 1);
+}
