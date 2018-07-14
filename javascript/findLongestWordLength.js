2080e988f0834678aeb2cacff330bbd9a44d91df
Marty A/V <martyavedon@gmail.com>
2018-07-13 17:28:33 -0700

    Create findLongestWordLength.js

---
+++ javascript/findLongestWordLength.js
@@ -0,0 +1,23 @@
+function findLongestWordLength(str) {
+  if (str.length === 0) {
+    return null;
+  }
+
+  let strArray = str.split(" ");
+
+   if (strArray.length === 1) {
+    return strArray.length;
+  }
+
+  let maxLength = Number.EPSILON;
+  
+  for (let word of strArray) {
+    let currentLength = word.length;
+
+    if (currentLength > maxLength) {
+      maxLength = currentLength;
+    }
+  }
+
+  return maxLength;
+}
