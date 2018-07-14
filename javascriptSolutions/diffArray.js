a0417a67a41e8ad901fc814b3fa411dd7df8a2b0
Marty A/V <martyavedon@gmail.com>
2018-07-14 02:12:19 -0700

    Create diffArray.js

---
+++ javascriptSolutions/diffArray.js
@@ -0,0 +1,19 @@
+function diffArray(arr1, arr2) {
+  let newArr = arr1.concat(arr2).sort();
+  let previous;
+  let current;
+
+  for (let i = 1; i < newArr.length; i++) {
+    current = newArr[i];
+    previous = newArr[i - 1];
+
+    if (previous === current) {
+      delete newArr[i - 1]; // deleting creates empty objects, which we must remove by filtering
+      delete newArr[i];
+    }
+  }
+
+  newArr = newArr.filter(function(a) { return a; });
+
+  return newArr;
+}
