c81ba92187932a36960bfa6eaed4724fd17e7008
Marty A/V <martyavedon@gmail.com>
2018-07-13 18:49:23 -0700

    added instructions and url

--- javascriptSolutions/truncateString.js
+++ javascriptSolutions/truncateString.js
@@ -1,3 +1,8 @@
+// From https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/basic-algorithm-scripting/truncate-a-string
+//
+// Truncate a string (first argument) if it is longer than the given maximum string length (second argument). 
+// Return the truncated string with a ... ending.
+
 function truncateString(str, num) {
   if (num === 0) {
     return "";
