f1d3623f78e9ff2e5706713f785cb98b73fc9273
Marty A/V <martyavedon@gmail.com>
2018-08-08 04:28:43 -0700

    Create rot13.js

---
+++ javascriptSolutions/rot13.js
@@ -0,0 +1,36 @@
+/* 
+One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. In a shift cipher the meanings of the letters are shifted by some set amount.
+
+A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus 'A' ↔ 'N', 'B' ↔ 'O' and so on.
+
+Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+
+All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
+*/
+
+function rot13(str) {
+    // we could memorize obscure unicode values or... 
+    let alphabetFront = "ABCDEFGHIJKLM";
+    let alphabetBack = "NOPQRSTUVWXYZ";
+    let isLetter = /[^\W^\d]/;
+
+    for (var i = 0; i < str.length; i++) {
+        let charToAdd = str[i];
+
+        if (isLetter.test(charToAdd)) {
+          let index;
+          
+          if (alphabetFront.includes(charToAdd)) {
+            index = alphabetFront.indexOf(charToAdd);
+            charToAdd = alphabetBack[index];
+          } else {
+            index = alphabetBack.indexOf(charToAdd);
+            charToAdd = alphabetFront[index];
+          }
+        }
+
+        str = charToAdd;
+    }
+
+    return str;
+}
