4bb4ed98670eca3b16754f0f8689e9c4496a3bd0
Marty A/V <martyavedon@gmail.com>
2018-08-15 21:45:25 -0700

    Create downRotate.js

---
+++ javascriptSolutions/downRotate.js
@@ -0,0 +1,21 @@
+ function downRotate(arr, shift) {
+   try {
+     arr.every(row => row.constructor === Array)
+   } catch(error) {
+     throw new TypeError("First arg must be an array of arrays");
+   }
+
+   try {
+     let check = arr[1];
+     check.constructor === Array;
+   } catch(error) {
+     throw new Error("First arg must be a multi-dimensional array which contains more than one row");
+   }
+
+   const end = arr.length;
+   const moveLastIndex = Math.abs((end - shift) % end);
+   const frontArr = arr.splice(moveLastIndex);
+   const backArr = arr.splice(0, moveLastIndex);
+
+   return frontArr.concat(backArr);
+ };
