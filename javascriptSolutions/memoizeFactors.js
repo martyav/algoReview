b8487cf686cdb8b868b924e47c867f30ae446ed7
Marty A/V <martyavedon@gmail.com>
2018-08-26 14:47:11 -0700

    removed redundant call

--- javascriptSolutions/memoizeFactors.js
+++ javascriptSolutions/memoizeFactors.js
@@ -5,7 +5,6 @@ function memoizeFactors(n, arr = []) {
     
   if (arr.length === 0 || arr[0] % n === 0) {
     arr.push(n)
-    return memoizeFactors(n - 1, arr);
   }
   
   return memoizeFactors(n - 1, arr);
