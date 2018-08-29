24154bdd900ef37a1bf76b0d1fbe8aa153bf0eca
Marty A/V <martyavedon@gmail.com>
2018-08-28 22:54:13 -0700

    spacing in comment

--- javascriptSolutions/calculateYears.js
+++ javascriptSolutions/calculateYears.js
@@ -1,11 +1,13 @@
 /*
-  https://www.codewars.com/kata/563f037412e5ada593000114/train/javascript
+From https://www.codewars.com/kata/563f037412e5ada593000114/train/javascript
   
-  Mr. Scrooge has a sum of money that he wants to invest, and he wants to know how many years this sum has to be kept in the bank in order for this sum of money to amount to a certain target value.
+Mr. Scrooge has a sum of money that he wants to invest, and he wants to know how many years this sum has
+to be kept in the bank in order for this sum of money to amount to a certain target value.
 
-  The sum is kept for n years in the bank where interest is paid yearly, and the new sum is re-invested yearly after paying tax.
+The sum is kept for n years in the bank where interest is paid yearly, and the new sum is re-invested 
+yearly after paying tax.
 
-  Note that the principal is not taxed but only the year's accrued interest
+Note that the principal is not taxed but only the year's accrued interest
 */
 
 function calculateYears(principal, interest, tax, desired) {
