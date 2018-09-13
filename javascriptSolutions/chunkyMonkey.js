57f3edefd44ad7e12206c90dfb0d66e8b546bf1d
Marty A/V <martyavedon@gmail.com>
2018-09-13 19:40:51 -0400

    edge case checking

--- javascriptSolutions/chunkyMonkey.js
+++ javascriptSolutions/chunkyMonkey.js
@@ -2,6 +2,18 @@
 // and returns them as a two-dimensional array.
 
 function chunkArrayInGroups(arr, size) {
+  if (size === 0) {
+    throw new RangeError("Size must be greater than 0.")
+  }
+
+  if (!arr || arr.length < 1) {
+    throw new Error("We must have an array conntaing elements passed in an argument.")
+  }
+
+  if (arr.length < size) {
+    throw new RangeError("The second argument must be equal to or less than the length of the array");
+  }
+  
   var returnArray = [];
 
   for (let i = 0; i < arr.length; i += size) {
