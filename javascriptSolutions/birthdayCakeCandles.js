f0996c645ce8143976684c280239458991f1cbe8
Marty A/V <martyavedon@gmail.com>
2018-09-19 13:18:49 -0400

    Create birthdayCakeCandles.js

---
+++ javascriptSolutions/birthdayCakeCandles.js
@@ -0,0 +1,11 @@
+/*
+https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+
+You are in charge of the cake for your niece's birthday and have decided the cake will have one candle for 
+each year of her total age. When she blows out the candles, she’ll only be able to blow out the tallest ones.
+Your task is to find out how many candles she can successfully blow out.
+*/
+
+function birthdayCakeCandles(ar) {    
+    return ar.filter(x => x === Math.max(...ar)).length;
+}
