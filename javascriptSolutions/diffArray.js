c72269d49994001158ddc336ee5eccd9ca9cb95b
Marty A/V <martyavedon@gmail.com>
2018-09-24 14:45:17 -0400

    Update diffArray.js

--- javascriptSolutions/diffArray.js
+++ javascriptSolutions/diffArray.js
@@ -17,3 +17,7 @@ function diffArray(arr1, arr2) {
 
   return newArr;
 }
+
+function diffArrayAlt(arr1, arr2) {
+  return arr1.concat(arr2).filter(function(a) { return !arr1.includes(a) || !arr2.includes(a); });
+}
