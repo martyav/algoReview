c087b677c24a634436e51a6f771120d84557b1f8
Marty A/V <martyavedon@gmail.com>
2018-09-24 13:49:50 -0400

    refactored for space and legibility
    also started using concat in preference to += because using + on anything other than numbers is a code smell and can lead to strange behavior

--- javascriptSolutions/oxfordCommaVampireWeekend.js
+++ javascriptSolutions/oxfordCommaVampireWeekend.js
@@ -1,47 +1,27 @@
 // From https://www.codewars.com/kata/format-a-string-of-names-like-bart-lisa-and-maggie/train/javascript
 
 function list(names) {
-    if (!Array.isArray(names)) {
-        throw new TypeError("Argument needs to be an array");
-    }
+  let returnString = "";
 
-    if (names.length < 1) {
-        return "";
-    }
+  if (!Array.isArray(names)) throw new TypeError("Argument needs to be an array");
+  if (typeof names[0] != "object" && names.length > 0) throw new TypeError("Argument needs to be an array of objects");
+  if (names.length === 1 && names[0].hasOwnProperty("name")) returnString = names[0].name;
+  if (names.length < 2) return returnString;
+
+  for (let i = 0; i < names.length; i++) {
+    if (!names[i].hasOwnProperty("name")) continue; 
     
-    if (typeof names[0] != "object") {
-      throw new TypeError("Argument needs to be an array of objects");
+    let individual = names[i].name;
+    let secondToLast = names.length - 2;
+
+    if (i < secondToLast) {
+      returnString = returnString.concat(`${ individual }, `);
+    } else if (i === secondToLast) {
+      returnString = returnString.concat(`${ individual } & `); 
+    } else {
+      returnString = returnString.concat(individual);
     }
+  }
 
-    if (names.length === 1) {
-        let onlyIndividual = names[0];
-
-        if (onlyIndividual.hasOwnProperty("name")) {
-            return onlyIndividual.name;
-        }
-    }
-
-    let returnString = "";
-
-    for (let i = 0; i < names.length; i++) {
-        let individual = names[i];
-        let triggerForDifferentFormating = names.length - 2;
-        let needsAmpersand = names.length - 2;
-
-        if (individual.hasOwnProperty("name")) {
-            let formattedName = individual.name;
-
-            if (i < triggerForDifferentFormating) {
-                formattedName += ", ";
-            } else {
-                if (i === needsAmpersand) {
-                    formattedName += " & ";
-                }
-            }
-
-            returnString += formattedName;
-        } 
-    }
-
-    return returnString;
+  return returnString;
 }
