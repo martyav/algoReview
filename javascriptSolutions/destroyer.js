6ee9afce8ed916b6db3add51d921b510d3bd934a
Marty A/V <martyavedon@gmail.com>
2018-09-24 14:02:41 -0400

    refactored with fat arrow

--- javascriptSolutions/destroyer.js
+++ javascriptSolutions/destroyer.js
@@ -7,9 +7,5 @@ function destroyer(arr) {
   // Arguments contains all the arguments passed to a method. We need to spread it, turn it into an array, then slice it after the first arg
   let args = Array(...arguments).slice(1); 
 
-  arr = arr.filter(function(a) {
-    return !args.includes(a);
-  });
-
-  return arr;
+  return arr.filter(a => !args.includes(a));
 }
