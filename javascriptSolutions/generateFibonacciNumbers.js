a265a19c4cec7f85bb11487d5b9011e47a80cd8c
Marty A/V <martyavedon@gmail.com>
2018-09-27 22:28:51 -0400

    teranary instead of if else

--- javascriptSolutions/generateFibonacciNumbers.js
+++ javascriptSolutions/generateFibonacciNumbers.js
@@ -1,8 +1,7 @@
 function generateFibonacciNumbers(limit, arr = []) {
   if (!limit) return arr;
 
-  if (arr.length < 2) arr.unshift(1);
-  else arr.unshift(arr[0] + arr[1]);
+  arr.length < 2 ? arr.unshift(1) : arr.unshift(arr[0] + arr[1]);
 
   return generateFibonacciNumbers(limit - 1, arr);
 }
